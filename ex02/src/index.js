4ac666ac1b6ecd6131530b3b29905f49dc4a1f4b
Ines1302 <seeking.knowledge13@gmail.com>
2021-03-10 14:45:11 +0100

    Create index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main () {
+
+    // Only change code below this line
+    // Ovo je moj drugi zadatak 
+    /* Danas nam je treci dan Arene. 
+    Zadaci su interesantni i veliko hvala mentorima :)  */
+
+    // Only chnage code above this line
+
+}
+
+console.log (main()) ;
+module.exports = main; 
